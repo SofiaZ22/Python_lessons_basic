1009bfcf8c4e559652b06fcf6ac99457ad8f52d1
SofiaZ22 <sofia.student21@gmail.com>
2019-07-02 15:34:36 +0500

    Добавлено решение

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,11 +1,20 @@
-# Задание-1:
+﻿# Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
-
+print('Задание-1')
 def my_round(number, ndigits):
-    pass
+    int_ndigits = int(ndigits)
+    degree = pow(10,int(ndigits))
+    mul =  number*degree
+    res = int(mul)
+    ost = mul-res
+    # print(number,mul,res,ost)
+    if not (abs(ost) < 0.5):
+        if res>0: res+=1
+        else: res-=1
+    return res/degree
 
 
 print(my_round(2.1234567, 5))
@@ -18,9 +27,18 @@ print(my_round(2.9999967, 5))
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
-
+print('Задание-2')
 def lucky_ticket(ticket_number):
-    pass
+    if (len(str(ticket_number))!= 6) or (type(ticket_number) is not int):
+        return 'Некорректный номер билета'
+    else:
+        ticket_number1 = list(str(ticket_number))
+        sum1 = int(ticket_number1[0]) + int(ticket_number1[1]) + int(ticket_number1[2])
+        sum2 = int(ticket_number1[3]) + int(ticket_number1[4]) + int(ticket_number1[5])
+        if sum1 == sum2:
+            return 'Билет %s счастливый' %ticket_number
+        else:
+            return 'Билет %s несчастливый' %ticket_number
 
 
 print(lucky_ticket(123006))
