b2f127a54d8037a3f50b9b8ff7df2df55788e924
SofiaZ22 <sofia.student21@gmail.com>
2019-07-05 15:02:43 +0500

    Добавлено решение 2 из 3 задач

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,4 @@
+import re
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -19,8 +20,25 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+print('Задание-1: Решение с помощью re')
+line_str = re.findall(r'[a-z]+', line)
+print('Символы в нижнем регистре с использованием модуля re: \n',line_str)
+print()
+print('Задание-1: Решение без re')
+symbol = list(map(lambda x: chr(x), list(range(65, 91))))  # Список из кодов ANSI преобразуем в список букв A-Z
+line_new = list(line)
+
+for i, el in enumerate(line_new[:]):
+       for el_2 in symbol:
+              if el == el_2:
+                     line_new[i] = ' '
 
 
+str = ''.join(line_new).split(' ')  # Соединение списка в строку методом .join и разбиение строки по символу ' '
+
+line_str_2 = [i for i in str if i != '']
+print('Символы в нижнем регистре без использованием модуля re: \n', line_str_2)
+print()
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -44,7 +62,49 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+print('Задание-2: Решение с помощью re')
+line_2_str = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print('Список с использованием модуля re: \n',line_2_str)
+print()
 
+print('Задание-2: Решение без re')
+symbol_1 = list(map(lambda x: chr(x), list(range(65, 91))))  # Преобразуем список из кодов ANSI в список букв A-Z
+symbol_2 = list(map(lambda x: chr(x), list(range(97, 123))))  # Преобразуем список из кодов ANSI в список букв a-z
+line_new = list(line_2)
+
+lst = []
+i = len(line_new) - 1
+# Находим  символ в верхнем регистре после которого стоят еще два символа в верхнем регистре
+while i >= 0:
+       if line_new[i] in symbol_2:
+              lst.append(line_new[i])
+       elif line_new[i] in symbol_1 and i <= len(line_new) - 3 and line_new[i + 1] in symbol_1 and line_new[
+              i + 2] in symbol_1:
+              lst.append(line_new[i])
+       else:
+              lst.append(' ')
+       i -= 1
+lst.reverse()  # Переворачиваем список
+
+i = 0
+lst2 = []
+v_reg = True  # Начальное условие поиска сортировки символов в верхнем регистре
+# Фильтрация списка.
+while i <= len(lst) - 1:
+       if lst[i] in symbol_2:
+              v_reg = True
+       if lst[i] in symbol_1 and lst[i - 1] in symbol_2 and lst[i - 2] in symbol_2:
+              lst2.append(lst[i])
+              v_reg = False
+       elif lst[i] in symbol_1 and v_reg == False:
+              lst2.append(lst[i])
+       else:
+              lst2.append(' ')
+       i += 1
+stroka = ''.join(lst2).split(' ')  # Преобразование в строку и разбиение по пробелам
+
+line_str_3 = [i for i in stroka if i != '']  # Формирование выхрдного списка из строки
+print('Список без использованием модуля re: \n', line_str_3)
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
