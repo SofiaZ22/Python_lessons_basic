b1b62de00487935e8cee0be679ccd248fd83cfee
SofiaZ22 <sofia.student21@gmail.com>
2019-07-12 16:13:20 +0500

    Добавлено решение урока 5

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,40 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+import os
+import hw05_normal
+
+def change_dir (path):
+    try:
+        os.chdir(path)
+        return 'Успешно перешли в папку: {}'.format(path)
+    except FileNotFoundError:
+        return ('dir_{} - папки не существует'.format(path))
+
+def start ():
+    answer =''
+    while answer != '5':
+        print('----------------------------------------')
+        print('Текущая директория: ' + os.getcwd())
+        answer = input('Выберите пункт меню:\n'
+                       '1. Перейти в папку\n'
+                       '2. Помотреть содержимое текущей папки\n'
+                       '3. Удалить папку\n'
+                       '4. Создать папку\n'
+                       '5. Выход\n')
+        if answer =='5':
+            break
+        if answer == '1':
+            path_name = input('Укажите папку для перехода: ')
+            print(change_dir(path_name))
+        elif answer == '2':
+            Less5.list_dir()
+        elif answer == '3':
+            name = input('Введите имя удаляемой папки: ')
+            Less5.remove_dir(name)
+        elif answer == '4':
+            name = input('Введите имя новой папки: ')
+            Less5.make_dir(name)
+
+if __name__ == '__main__':
+    start()
