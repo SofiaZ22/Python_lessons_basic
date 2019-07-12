c23cb1ebea1c1d64f2e489d625404e47e7b2bfa8
SofiaZ22 <sofia.student21@gmail.com>
2019-07-12 14:33:12 +0500

    Решение 1 задачи

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,9 +1,48 @@
-# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
+﻿# Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
 
+import math
 
+
+class Triangle:
+    def __init__(self, A, B, C):
+        # Функция вычисляет длину стороны согласно координатам точек
+        def sideLen(dot1, dot2):
+            return math.sqrt((dot1[0] - dot2[0]) ** 2
+                             + (dot1[1] - dot2[1]) ** 2)
+
+        self.A = A
+        self.B = B
+        self.C = C
+        # На основании соседних координат вычисляем длину стороны AB
+        self.AB = sideLen(self.A, self.B)
+        self.BC = sideLen(self.B, self.C)
+        self.CA = sideLen(self.C, self.A)
+
+    # Вычисление площади треугольника по формуле Герона
+    def areaTriangle(self):
+        semi_perimeter = self.perimeterTriangle() / 2
+
+        return math.sqrt(semi_perimeter
+                         * (semi_perimeter - self.AB)
+                         * (semi_perimeter - self.BC)
+                         * (semi_perimeter - self.CA))
+
+    # вычисляем периметр треугольника
+    def perimeterTriangle(self):
+        return self.AB + self.BC + self.CA
+
+    # Вычисляем высоту треугольника
+    def heightTriangle(self):
+        return self.areaTriangle() / (self.AB / 2)
+
+
+treugolnik1 = Triangle((3, 2), (6, 7), (0, 12))
+
+print('Площадь:', treugolnik1.areaTriangle())
+print('Высота:', treugolnik1.heightTriangle())
+print('Периметр:', treugolnik1.perimeterTriangle())
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
-# Предусмотреть в классе методы:
-# проверка, является ли фигура равнобочной трапецией;
-# вычисления: длины сторон, периметр, площадь.
-
+# # Предусмотреть в классе методы:
+# # проверка, является ли фигура равнобочной трапецией;
+# # вычисления: длины сторон, периметр, площадь.
